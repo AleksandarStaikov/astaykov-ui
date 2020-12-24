90dbc5d14f14dbb98d675cc1b1fdc7b34a481a78
Aleksandar Staykov <astaykov@student.fontys.nl>
2020-12-25 00:23:26 +0200

    adding a bit of layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,54 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <nav>
+    <router-link to="/">
+      <div class="navigation__logo">Staiski</div>
+    </router-link>
+
+    <div class="navigation__left">
+      <router-link :to="{ name: 'Home' }">Home</router-link>
+      <router-link :to="{ name: 'About' }">About</router-link>
+      <router-link :to="{ name: 'Home' }">Zdraveiii</router-link>
+    </div>
+  </nav>
+  <router-view />
 </template>
 
-<style>
+<script>
+export default {
+  name: "App",
+  setup() {},
+};
+</script>
+
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px 5%;
+    background-color: deeppink;
+    
+    a {
+      text-decoration: none;
+      color: white;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    .navigation__logo {
+      font-weight: bold;
+      font-size: 24px;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+    .navigation__left a {
+      font-weight: normal;
+      padding: 0px 10px;
+    }
+  }
 }
 </style>
